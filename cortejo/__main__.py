f85400c91fdd949747d430f5eb43ad46eeb66cf4
Iwan van der Kleijn <iwanvanderkleijn@gmail.com>
2024-03-04 23:33:04 +0100

    Generate test cases for test 'Login' test case

--- cortejo/__main__.py
+++ cortejo/__main__.py
@@ -6,7 +6,9 @@ Cortejo - Create Cypress tests based on a human language definition (using AI)
 @license: MIT
 """
 
+from dataclasses import dataclass
 import os
+from typing import Dict, List
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 import pandas as pd
@@ -22,14 +24,15 @@ except Exception as e:
     exit(1)
 
 
+@dataclass
 class TestData:
-    def __init__(self, use_case, description, type, input_elements, action, expected_result):
-        self.use_case = use_case
-        self.description = description
-        self.type = type
-        self.input_elements = input_elements
-        self.action = action
-        self.expected_result = expected_result
+    #def __init__(self, use_case, description, type, input_elements, action, expected_result):
+    use_case: str
+    description: str
+    type: str
+    input_elements: str
+    action: str
+    expected_result: str
 
 
 def read_tests(filename):
@@ -41,12 +44,45 @@ def read_tests(filename):
     df = df[columns]
 
     # Convert the DataFrame rows to TestData objects
-    test_data_list = [TestData(row['Use Case'], row['Description'], row['Type'], row['Input Elements'], row['Action'], row['Expected Result']) for index, row in df.iterrows()]
+    test_data_list = [TestData(row['Use Case'], row['Description'], row['Type'], row['Input Elements'], row['Action'], 
+                               row['Expected Result']) for index, row in df.iterrows()]
 
     return test_data_list
 
 
+def split_tests_in_use_cases(test_data) -> Dict[str, List[TestData]]:
+
+    use_cases: Dict[str, List[TestData]] = {}
+    for test in test_data:
+        if test.use_case not in use_cases: 
+            use_cases[test.use_case] = []
+        use_cases[test.use_case].append(test)
+    return use_cases
+
+
+def generate_test_prompt(use_case: str, test_data: list[TestData]) -> str:
+    templates = [f"""For the Use Case "{use_case}", create a series of Cypress tests. Do not include
+the cy.visit call to each tests. But rather, include it in the beforeEach block. 
+For the tests use the following data representing each test case:
+    """]
+    
+    for data in test_data:
+        templates.append(f"""Description :- {data.description}	
+Type :- {data.type}	
+Input Elements :- {data.input_elements}
+Action :- {data.action}	
+Expected Result :- {data.expected_result}
+""")
+        
+    return "\n".join(templates)
+    
+def generate_cypress_test(use_case: str, test_data: list[TestData]) -> str:
+    prompt = generate_test_prompt(use_case, test_data)
+    response = llm.invoke(prompt)
+    return response.content # type: ignore
+
+
 if __name__ == '__main__':
     test_data = read_tests('data/test-data.xlsx')
-    for test in test_data:
-        print(test.use_case, test.description)
+    use_cases = split_tests_in_use_cases(test_data) 
+    print(generate_cypress_test('Login', use_cases['Login']))
